7ca672490103ae7eec21c95ad82ad16644b279da
Kye <kyegiovannoni2@gmail.com>
2023-02-28 17:58:15 +0000

    change to films and actors

--- GraphQLAPI/models/_person.py
+++ GraphQLAPI/models/_person.py
@@ -6,16 +6,36 @@ import datetime
 from GraphQLAPI.utils.directives import Keys
 
 DATA_LOOKUP = {
-  "persons":[
+  "actors":[
       dict(
          id = 1
         ,first_name= 'Jeff'
         ,last_name = 'Bridges'
-        ,age= 63
+        ,age= 73
         ,admin= 'True'
-        ,email= 'jeffB@hotmail.com'
-        ,created_on= datetime.datetime.now()
-        ,tag = uuid.uuid4()
+        ,height= 185
+        ,data_created_on= datetime.datetime.now()
+        ,uniqueId = uuid.uuid4()
+   ), 
+   dict(
+         id = 3
+        ,first_name= 'John'
+        ,last_name = 'Goodman'
+        ,age= 70
+        ,admin= 'True'
+        ,height= 188
+        ,data_created_on= datetime.datetime.now()
+        ,uniqueId = uuid.uuid4()
+   ),
+   dict(
+         id = 4
+        ,first_name= 'Julianne'
+        ,last_name = 'Moore'
+        ,age= 62
+        ,admin= 'True'
+        ,height= 160
+        ,data_created_on= datetime.datetime.now()
+        ,uniqueId = uuid.uuid4()
    ),
     dict(
          id = 2
@@ -23,83 +43,113 @@ DATA_LOOKUP = {
         ,last_name = 'Goldblum'
         ,age= 71
         ,admin= 'True'
-        ,email= 'jeffGB@hotmail.com'
-        ,created_on= datetime.datetime.now()
-        ,tag = uuid.uuid4()
+        ,height= 180
+        ,data_created_on= datetime.datetime.now()
+        ,uniqueId = uuid.uuid4()
    )
   ],
-    "posts":[
+    "films":[
       dict(
          id = 1
-        ,title= 'what is GraphQL? '
-        ,authorId= 1
-        ,created_on= datetime.datetime.now()
+        ,title= 'The Big Lebowski'
+        ,actorId= 1
+        ,date= datetime.date(1998,5,1)
    ),
     dict(
          id = 2
-        ,title= '1+1=3?'
-        ,authorId= 1
-        ,created_on= datetime.datetime.now()
-   )
+        ,title= 'The Fly'
+        ,actorId= 2
+        ,date= datetime.date(1986,3,23)
+   ), dict(
+         id = 3
+        ,title= 'Hell or High Water'
+        ,actorId= 1
+        ,date= datetime.date(2016,1,12)
+   ),
+  ],
+  "actorFilms":[
+   dict(
+    actorId=1,
+    filmId= 1
+   ),
+   dict(
+    actorId=3,
+    filmId= 1
+   ),
+   dict(
+    actorId=4,
+    filmId= 1
+   ),
+   dict(
+    actorId=1,
+    filmId= 3
+   ),
+    dict(
+    actorId=2,
+    filmId= 2
+   ),
   ]
 }
 
 #resolvers
-async def get_persons(root:"Person", info: Info, name:typing.Optional[str]=None)-> typing.List["Person"]:
+async def get_actors(root:"Actor", info: Info, name:typing.Optional[str]=None)-> typing.List["Actor"]:
   if name:
-     return [Person(**person) for person in DATA_LOOKUP['persons'] if person['first_name'].lower() == name.lower() ]  
-  return [Person(**person) for person in DATA_LOOKUP['persons']]   
+     return [Actor(**actor) for actor in DATA_LOOKUP['actors'] if actor['first_name'].lower() == name.lower() ]  
+  return [Actor(**actor) for actor in DATA_LOOKUP['actors']]   
 
-async def get_person(root:"Person", info: Info, id:int) -> "Person":
-    _person=[Person(**person) for person in DATA_LOOKUP['persons'] if DATA_LOOKUP['id'] == id ]
-    return _person[0]
+async def get_actor(root:"Actor", info: Info, id:int) -> "Actor":
+    _actor=[Actor(**actor) for actor in DATA_LOOKUP['actors'] if actor['id'] == id ]
+    return _actor[0]
 
-async def get_person_for_post(root:"Post", info: Info) -> "Person":
-    _person=[Person(**person) for person in DATA_LOOKUP['persons'] if person['id'] == root.authorId ]
-    return _person[0]
+async def get_actors_for_film(root:"Film", info: Info) -> typing.List["Actor"]:
+    actor_ids = [mapping['actorId'] for mapping in DATA_LOOKUP['actorFilms'] if mapping["filmId"] == root.id] 
+    _actors=[Actor(**actor) for actor in DATA_LOOKUP['actors'] if actor['id'] in actor_ids]
+    return _actors
 
-async def full_name(root: "Person", info: Info) -> str: 
+async def full_name(root: "Actor", info: Info) -> str: 
     return f"{root.first_name} {root.last_name}"#{info.field_name}"
 
-async def get_post(root: "Post", info: Info, id:int) -> typing.List["Post"]:
-  post=[Post(**post) for post in DATA_LOOKUP['posts'] if post['id'] == id]
-  return post[0]
+async def get_film(root: "Film", info: Info, id:int) -> typing.List["Film"]:
+  film=[Film(**film) for film in DATA_LOOKUP['films'] if film['id'] == id]
+  return film[0]
 
-async def get_posts(root: "Post", info: Info) -> typing.List["Post"]:
-  posts=[Post(**post) for post in DATA_LOOKUP['posts']]
-  return posts
+async def get_films(root: "Film", info: Info) -> typing.List["Film"]:
+  films=[Film(**film) for film in DATA_LOOKUP['films']]
+  return films
 
-async def get_posts_for_author(root: "Person", info: Info ) -> "Post":
-  posts = [Post(**post) for post in DATA_LOOKUP['posts'] if post['authorId'] == root.id ]   
-  return posts
+async def get_films_for_actor(root: "Actor", info: Info ) -> "Film":
+  film_ids = [mapping['filmId'] for mapping in DATA_LOOKUP['actorFilms'] if mapping["actorId"] == root.id] 
+  films = [Film(**film) for film in DATA_LOOKUP['films'] if film['id'] in film_ids]   
+  return films
 
 @strawberry.type
-class Post:
-  authorId: strawberry.Private[str]
+class Film:
+  actorId: strawberry.Private[str]
   id: strawberry.Private[str]
   title: str
-  author: "Person" = strawberry.field(resolver=get_person_for_post)
-  created_on: datetime.datetime
+  actors: typing.List["Actor"] = strawberry.field(resolver=get_actors_for_film)
+  date: datetime.date
 
 ##schema 
 #@strawberry.type(directives=[Keys(fields="id")])
 @strawberry.type
-class Person:
+class Actor:
   id: strawberry.Private[str]
   first_name: strawberry.Private[str]
   last_name: strawberry.Private[str]
   full_name: str = strawberry.field(resolver=full_name)
   age: int
   admin: str
-  posts: typing.List["Post"] = strawberry.field(resolver=get_posts_for_author)
-  email: str
-  created_on: datetime.datetime
-  tag: uuid.UUID
+  films: typing.List["Film"] = strawberry.field(resolver=get_films_for_actor)
+  height: int
+  data_created_on: datetime.datetime
+  uniqueId: uuid.UUID
+
 
 @strawberry.type
-class PersonsQuery:
-    getPersons: typing.List[Person] = strawberry.field(resolver=get_persons) 
-    getPerson: Person = strawberry.field(resolver=get_person)
-    getAllPosts: typing.List[Post] = strawberry.field(resolver=get_posts)
+class FilmQuery:
+    getActors: typing.List[Actor] = strawberry.field(resolver=get_actors) 
+    getActor: Actor = strawberry.field(resolver=get_actor)
+    getAllFilms: typing.List[Film] = strawberry.field(resolver=get_films)
 
 
--- GraphQLAPI/models/schema.py
+++ GraphQLAPI/models/schema.py
@@ -7,14 +7,14 @@ from strawberry.tools import merge_types
 from os import environ
 
 # Graphql model query objects.
-from GraphQLAPI.models._person import PersonsQuery
+from GraphQLAPI.models._person import FilmQuery
 from GraphQLAPI.models._taxis import TaxisQuery
 from GraphQLAPI.models._payment_types import PaymentTypeQuery
 from GraphQLAPI.models._vendors import VendorQuery
 from GraphQLAPI.models._calculated import CalculatedQuery
 
 # Combine all root special Query types.
-BaseQuery = merge_types("BaseQuery", (TaxisQuery, PaymentTypeQuery, VendorQuery, PersonsQuery, CalculatedQuery) )
+BaseQuery = merge_types("BaseQuery", (TaxisQuery, PaymentTypeQuery, VendorQuery, FilmQuery, CalculatedQuery) )
 
 # Add base schema extensions.
 extensions = [
